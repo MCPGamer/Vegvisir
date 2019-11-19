7d1ccf910d29a4b2cb4fd2bca92426d87dd5f380
godu <duarte.goncalves@aeis.ch>
2019-11-19 07:56:47 +0100

    Added Standard Springboot Project

---
+++ Vegvisir/src/main/java/ch/bbw/Vegvisir/VegvisirApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.Vegvisir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegvisirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegvisirApplication.class, args);
+	}
+
+}
